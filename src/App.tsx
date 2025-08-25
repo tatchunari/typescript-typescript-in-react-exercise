1438bc69c89bfdaf79c5901d248cab98295ceb80
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-08-25 11:19:50 +0700

    feat: edit answer for ex1

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  <Greeting name={""} age={0} />
-  return;
+ return (
+    <Greeting name={""} age={0} />
+
+ )
 }
 
 export default App;
